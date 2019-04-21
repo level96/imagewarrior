f7ab81a387fa751a70a82c209a36ea0eed9dc580
Trung Phan <trung.phan@level96.de>
2019-04-21 17:01:52 +0200

    updated base-url

--- imagewarrior/django_compat/templatetags/imagewarrior.py
+++ imagewarrior/django_compat/templatetags/imagewarrior.py
@@ -1,5 +1,6 @@
 from django import template
 from django.utils.safestring import mark_safe
+from django.conf import settings
 
 from imagewarrior.imagewarrior import Resize
 from imagewarrior.imagewarrior import url
@@ -15,7 +16,8 @@ def image_src(img, dim, gravity=None, gravity_focus=None, resize_type=Resize.FIL
     width, height = dim.split('x')
     gfoc = gravity_focus.split(',') if gravity_focus else None
     bg = background.split(',') if background else None
-    return url(img, width=width, height=height, gravity=gravity, gravity_focus=gfoc, resize_type=resize_type,
+    full_url = '{}{}'.format(settings.BASE_URL, img)
+    return url(full_url, width=width, height=height, gravity=gravity, gravity_focus=gfoc, resize_type=resize_type,
                quality=quality, blur=blur, sharpen=sharpen, ext=ext, dpr=dpr, background=bg)
 
 
